a31dd8d71a72a70592ca4570224a1ee5964a2631
Denis Federico Zelaya <Denisfzelaya@gmail.com>
2023-03-19 16:07:51 -0600

    Integranto top tabs navigator

--- src/navigations/TopTabsNavigation.tsx
+++ src/navigations/TopTabsNavigation.tsx
@@ -1,12 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+
+const Tab = createMaterialTopTabNavigator();
+
+function Home1() {
+    return (
+        <Text>Home1</Text>
+    )
+
+}
+
+function Home2() {
+    return (
+        <Text>Home2</Text>
+    )
+
+}
 
 const TopTabsNavigation = () => {
-  return (
-    <View>
-      <Text>TopTabsNavigation</Text>
-    </View>
-  )
+    return (
+        <Tab.Navigator overScrollMode={"always"} screenOptions={{ lazy : true, tabBarScrollEnabled: true }}>
+            <Tab.Screen name="Home1" component={Home1} />
+            <Tab.Screen name="Home2" component={Home2} />
+            <Tab.Screen name="Home3" component={Home2} />
+        </Tab.Navigator>
+    )
 }
 
 export default TopTabsNavigation
