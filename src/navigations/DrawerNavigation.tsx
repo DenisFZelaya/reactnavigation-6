9db7ebf8dd2e3799aff12f02ab850dac4fb28adc
Denis Federico Zelaya <Denisfzelaya@gmail.com>
2023-03-19 15:46:41 -0600

    Drawer Funcional

--- src/navigations/DrawerNavigation.tsx
+++ src/navigations/DrawerNavigation.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import TopTabsNavigation from './TopTabsNavigation';
 
 function Feed() {
     return (
@@ -18,8 +19,9 @@ const Drawer = createDrawerNavigator();
 
 const DrawerNavigation = () => {
     return (
-        <Drawer.Navigator>
-            <Drawer.Screen name="Feed" component={Feed} />
+        <Drawer.Navigator
+            screenOptions={{ drawerActiveBackgroundColor: "gold", headerTitle: "Hello Crack!", headerShadowVisible: true }}>
+            <Drawer.Screen name="Feed" component={TopTabsNavigation} />
             <Drawer.Screen name="Article" component={Article} />
         </Drawer.Navigator>
     )
---
+++ src/navigations/TopTabsNavigation.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TopTabsNavigation = () => {
+  return (
+    <View>
+      <Text>TopTabsNavigation</Text>
+    </View>
+  )
+}
+
+export default TopTabsNavigation
