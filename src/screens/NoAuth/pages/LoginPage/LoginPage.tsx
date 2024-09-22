a21ebe93fdc36df5e9d8ac689263035c166d6258
davidcanaca <denisfzelaya@gmail.com>
2024-09-22 14:31:18 -0600

    feat: login

--- src/screens/NoAuth/pages/LoginPage/LoginPage.tsx
+++ src/screens/NoAuth/pages/LoginPage/LoginPage.tsx
@@ -1,17 +1,102 @@
-import { View, Text, ScrollView, Button } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Button, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 import { className } from '../../../../utils/className'
 
 export default function LoginPage({ navigation }) {
-  return (
-    <View>
-          <Text style={className("bg-gray-800 font-bold text-white")}>LoginPage</Text>
-          <ScrollView>
-            <Button title='Go to Login' onPress={() => navigation.navigate('Login')} />
-            <Button title='Go to Register' onPress={() => navigation.navigate('Register')} />
-            <Button title='Go to UpdatePassword' onPress={() => navigation.navigate('UpdatePassword')} />
-        </ScrollView>
-    </View>
-  )
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        // Aquí iría la lógica de inicio de sesión
+        console.log('Login with:', email, password);
+    };
+
+    //            <Button title='Go to Login' onPress={() => navigation.navigate('Login')} />
+    // <Button title='Go to Register' onPress={() => navigation.navigate('Register')} />
+    // <Button title='Go to UpdatePassword' onPress={() => navigation.navigate('UpdatePassword')} />
+
+    return (
+        <View
+            style={className("bg-white")}>
+            <ScrollView
+                contentContainerStyle={className("w-full max-w-md p-6 rounded-2xl")}>
+                <View style={className("text-center")}>
+                    <Text style={className("mt-6 text-3xl font-bold text-gray-900")}>Bienvenido de nuevo</Text>
+                    <Text style={className("mt-2 text-sm text-gray-600")}>Inicia sesión para continuar</Text>
+                </View>
+                <View style={className("mt-8")}>
+                    <View style={className("rounded-md")}>
+                        <TextInput
+                            style={className("rounded-t-md border border-gray-300 p-2")}
+                            placeholder="Correo electrónico"
+                            value={email}
+                            onChangeText={setEmail}
+                            // autoCompleteType="email"
+                            keyboardType="email-address"
+                            textContentType="emailAddress"
+                        // required
+                        />
+                        <TextInput
+                            style={className("rounded-b-md border border-gray-300 p-2")}
+                            placeholder="Contraseña"
+                            value={password}
+                            onChangeText={setPassword}
+                            secureTextEntry
+                            // autoCompleteType="password"
+                            textContentType="password"
+                        // required
+                        />
+                    </View>
+
+                    <TouchableOpacity
+                        onPress={handleLogin}
+                        style={className(" relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white")}
+                    >
+                        <Text style={className("absolute left-0 inset-y-0 flex items-center pl-3")}>
+                            Iniciar sesión
+                        </Text>
+
+                    </TouchableOpacity>
+                </View>
+
+                <View style={className("mt-6")}>
+                    <View style={className("relative")}>
+                        <View style={className("absolute inset-0 flex items-center")}>
+                            <View style={className("w-full border-t border-gray-300")} />
+                        </View>
+                        <View style={className("relative flex justify-center text-sm")}>
+                            <Text style={className("px-2 bg-white text-gray-500")}>O continúa con</Text>
+                        </View>
+                    </View>
+
+                    <View style={className("mt-6 flex flex-row justify-between")}>
+                        <TouchableOpacity
+                            style={className("w-full flex flex-row items-center justify-center border border-gray-300 rounded-md p-2")}>
+                            <Text style={className("ml-2")}>Facebook</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={className("w-full flex flex-row items-center justify-center border border-gray-300 rounded-md p-2")}>
+                            <Text style={className("text-red-600")}>Google</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={className("w-full flex flex-row items-center justify-center border border-gray-300 rounded-md p-2")}>
+                            <Text style={className("text-blue-500")}>Microsoft</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                <View style={className("flex flex-row justify-between")}>
+                    <TouchableOpacity onPress={() => {/* Lógica para olvidar contraseña */ }}>
+                        <Text style={className("font-medium")}>
+                            ¿Olvidaste tu contraseña?
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+                        <Text style={className("font-medium")}>
+                            Registrarse <Text style={className(" h-4 w-4")}>ICON</Text>
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </View>
+    )
 }
 
